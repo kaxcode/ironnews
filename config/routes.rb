d1ba9728ee2ba95e7ee41a74e8df9d2ab33f006e
Kenia Valladarez <keniavalladarez@gmail.com>
2016-12-09 14:27:40 -0500

    Added a controller for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :stories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "stories#index"
+  get '/login'  => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 end
